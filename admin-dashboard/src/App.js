632ac114299768c7cb0c1781733bb2273273dcfa
MeerUlHassan <github4421@gmail.com>
2024-02-07 02:27:11 -0800

    setting up pages and components

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
+import Home from "./Pages/home/Home";
+import Login from "./Pages/login/Login";
+import List from "./Pages/list/List";
+import Single from "./Pages/singleUser/Single";
+import New from "./Pages/new/New";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="users">
+          <Route index element={<List />} />
+          <Route path=":userid" element={<Single />} />
+          <Route path="new" element={<New />} />
+        </Route>
+        <Route path="product">
+          <Route index element={<List />} />
+          <Route path=":productid" element={<Single />} />
+          <Route path="new" element={<New />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
---
+++ admin-dashboard/src/Pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../home/home.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Home;
