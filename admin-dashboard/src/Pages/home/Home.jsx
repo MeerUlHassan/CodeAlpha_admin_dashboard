91d3e32b9921ce0f0c1abfcfa2680de0b3727de2
MeerUlHassan <github4421@gmail.com>
2024-02-08 04:23:06 -0800

    navbar ui done

--- admin-dashboard/src/Pages/home/Home.jsx
+++ admin-dashboard/src/Pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../home/home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homecontainer">Container</div>
+      <div className="homecontainer">
+        <Navbar />
+        Home Container
+      </div>
     </div>
   );
 };
