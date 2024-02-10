1a44f8dcb39dc95e2a0fa06dcea2a43e3cece940
MeerUlHassan <github4421@gmail.com>
2024-02-10 05:16:54 -0800

    mui table added

--- admin-dashboard/src/Pages/home/Home.jsx
+++ admin-dashboard/src/Pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/tables/Table";
 
 const Home = () => {
   return (
@@ -13,14 +14,18 @@ const Home = () => {
       <div className="homecontainer">
         <Navbar />
         <div className="widgets">
-          <Widget type='user' />
-          <Widget type='order' />
-          <Widget type='earning' />
-          <Widget type='balance' />
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
         <div className="charts">
           <Featured />
           <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transections</div>
+          <Table />
         </div>
       </div>
     </div>
